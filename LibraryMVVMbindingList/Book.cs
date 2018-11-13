79d3d4daa2f77a5fe7673712756070e0b3d5e841
NKragh <39466838+NKragh@users.noreply.github.com>
2018-11-13 09:12:43 +0100

    Initial commit

---
+++ LibraryMVVMbindingList/Book.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryMVVMbindingList.Annotations;
+
+namespace LibraryMVVMbindingList
+{
+    class Book : INotifyPropertyChanged
+    {
+        private string _isbn;
+        private string _author;
+        private string _title;
+        private string _loaner;
+
+        public Book(String isbn, string author, string title)
+        {
+            _author = author;
+            _isbn = isbn;
+            _title = title;
+        }
+
+        public string Isbn
+        {
+            get { return _isbn; }
+            set { _isbn = value; }
+        }
+
+        public string Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string Loaner
+        {
+            get { return _loaner; }
+            set
+            {
+                _loaner = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("title:");
+            sb.Append(Title);
+            sb.Append("\n");
+            sb.Append("loaner:");
+            sb.Append(Loaner);
+            return sb.ToString();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ LibraryMVVMbindingList/ViewModel.cs
@@ -0,0 +1,83 @@
+﻿using GalaSoft.MvvmLight.Command;
+using LibraryMVVMbindingList.Annotations;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace LibraryMVVMbindingList
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+
+        private string _loaner;
+
+        // her huskes den valgte bog :-)
+        private Book _selectedBook;
+
+        // Her gemmes alle bøger - observable coolection betyder at den kan notificere GUI om ændringer
+        private ObservableCollection<Book> _books = new ObservableCollection<Book>();
+
+        public ViewModel()
+        {
+            _books.Add(new Book("1234567654321-1234", "Ebbe Vang", "Flæskesteg for begyndere"));
+            _books.Add(new Book("2345-76543-123", "Lars trOlesen", "Frokost på nye måder"));
+            _books.Add(new Book("123456-76543-432", "Lasse Coinbæk", "Bitcoin i Vipperød"));
+            _books.Add(new Book("12345-765432-12345", "Michael Kærgården", "Den der smører godt, kører godt - Glæden ved at CYKLE"));
+
+            LoanBookCommand = new RelayCommand(
+                addLoanertoSelectedBook);
+
+        }
+
+        public Book SelectedBook
+        {
+            get { return _selectedBook; }
+            set
+            {
+                _selectedBook = value;
+                OnPropertyChanged(); // fortæller XAML at property er opdateret
+            }
+        }
+
+        public ObservableCollection<Book> Books
+        {
+            get { return _books; }
+            set { _books = value; }
+        }
+
+        public string Loaner
+        {
+            get { return _loaner; }
+            set
+            {
+                _loaner = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand LoanBookCommand
+        {
+            get;
+            private set;
+        }
+
+        private void addLoanertoSelectedBook()
+        {
+            Debug.WriteLine("add loaner");
+            //Debug.WriteLine("Loaner: " + Loaner);
+            //Debug.WriteLine("SelectedBook_: "+ SelectedBook.Title);
+            Debug.WriteLine(SelectedBook);
+            SelectedBook.Loaner = Loaner;
+            //OnPropertyChanged("SelectedBook");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
